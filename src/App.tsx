2cc9fa964754246c3886be2c234ed76c821a0933
ifeanyi <omejuaifeanyi2@gmail.com>
2024-08-15 22:09:16 +0000

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-
+import Navbar from './components/Navber';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
+import Home from './pages/Home';
+import Exercise from './pages/Exercise';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Box width='400px'>
-      Navber
+    <Box width='400px' sx={{ width: {xl: '1200px'}}} m='auto'>
+      <Navbar/>
       <Routes>
-        <Route path='/' />
-        <Route path='/' />
+        <Route path='/' element={<Home/>}/>
+        <Route path='/exercise/:id' element={<Exercise/>}/>
       </Routes>
+      <Footer/>
     </Box>
   );
 }
--- src/components/Navber.tsx
+++ src/components/Navber.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png'
 
 const Navber = () => {
   return (
-    <div>Navber</div>
+    <Stack direction='row' justifyContent='space-around' sx={{gap: {sm: '40px', xl: '122px'}, mt: {sm: '20px', xs: '14px'}, justifyContent:'none', px: '10px'}} >
+      <Link to='/'>
+        <img src={Logo} alt='Logo' style={{ width: '43px', height: '43px', margin: '0px 20px' }}/>
+      </Link>
+
+      <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
+        <Link to='/' style={{ textDecoration: 'none', color: '#3a1212', borderBottom: '1px solid #ff2625' }}>Home</Link>
+        <a href='#excercise' style={{ textDecoration: 'none', color: '#3a1212' }}> 
+        Excercise
+        </a>
+      </Stack>
+    </Stack>
   )
 }
 
